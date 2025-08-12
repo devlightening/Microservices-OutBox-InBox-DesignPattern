6e3f3429e3d19d2e22361f122d84219f4bda7d77
Halil Yildirim <ce.hybusiness@gmail.com>
2025-08-12 23:51:42 +0300

    Revisions have been made to the Outbox Table implementation. The OrderOutboxTablePublisherService now checks for new OrderCreatedEvent requests every 5 seconds. These incoming requests are processed, and the corresponding data is stored in the database for reliable publishing.

--- OrderOutboxTablePublisherService/Jobs/OrderOutboxPublishJob.cs
+++ OrderOutboxTablePublisherService/Jobs/OrderOutboxPublishJob.cs
@@ -1,17 +1,43 @@
-﻿using Quartz;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MassTransit;
+using OrderOutboxTablePublisherService;
+using OrderOutboxTablePublisherService.Entites;
+using Quartz;
+using Shared.Events;
+using System.Text.Json;
 
-namespace OrderOutboxTablePublisherService.Jobs
+namespace Order.Outbox.Table.Publisher.Service.Jobs
 {
-    public class OrderOutboxPublishJob : IJob
+    public class OrderOutboxPublishJob(IPublishEndpoint publishEndpoint) : IJob
     {
         public async Task Execute(IJobExecutionContext context)
         {
-             Console.WriteLine("Tetiklendi.." + DateTime.UtcNow.Second);
+            // Veritabanı durumu kontrolü ve meşgul etme/serbest bırakma
+            if (OrderOutboxSingletonDatabase.DataReaderState)
+            {
+                OrderOutboxSingletonDatabase.DataReaderBusy();
+
+                // Doğru sütun adıyla (ProcessedDate) sorgu yapma
+                List<OrderOutbox> orderOutboxes = (await OrderOutboxSingletonDatabase.QueryAsync<OrderOutbox>($@"SELECT * FROM ORDEROUTBOXES WHERE ProcessedDate IS NULL ORDER BY OccuredOn ASC")).ToList();
+
+                foreach (var orderOutbox in orderOutboxes)
+                {
+                    // Yalnızca OrderCreatedEvent tipindeki mesajları işle
+                    if (orderOutbox.Type == nameof(OrderCreatedEvent))
+                    {
+                        OrderCreatedEvent? orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderOutbox.Payload);
+                        if (orderCreatedEvent != null)
+                        {
+                            await publishEndpoint.Publish(orderCreatedEvent);
+
+                            // Güncelleme işlemini güvenli bir şekilde yapma
+                            await OrderOutboxSingletonDatabase.ExecuteAsync($"UPDATE ORDEROUTBOXES SET ProcessedDate = GETDATE() WHERE IdempotentToken = '{orderOutbox.IdempotentToken}'");
+                        }
+                    }
+                }
+
+                OrderOutboxSingletonDatabase.DataReaderReady();
+                await Console.Out.WriteLineAsync("Order outbox table checked!");
+            }
         }
     }
 }
